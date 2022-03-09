d2a676305dac2afd49cb39e6f93d9d885212e35a
husakki <hueschromo@gmail.com>
2022-03-09 17:40:36 +0100

    created tmp dicitionary | added get methods for the cars

--- backend.py
+++ backend.py
@@ -1,4 +1,50 @@
 from fastapi import FastAPI
 
+# COFM = Car of the Month
+tmpdict = {
+    1: {
+        "Marke": "Mercedes",
+        "Model": "A-Klasse",
+        "Kilometer": 200.000,
+        "Verbrauch": 5.2,
+        "Preis": 10.000,
+        "COFM": 1
+    },
+    2: {
+        "Marke": "Mercedes",
+        "Model": "C-Klasse",
+        "Kilometer": 30.000,
+        "Verbrauch": 9.3,
+        "Preis": 50.000,
+        "COFM": 0,
+    },
+    3: {
+        "Marke": "Mercedes",
+        "Model": "E-Klasse",
+        "Kilometer": 60.000,
+        "Verbrauch": 15.0,
+        "Preis": 70.000,
+        "COFM": 0,
+    }
+}
+
+app = FastAPI()
 
 
+# add car
+
+# delete car
+
+# update car
+
+# get cars
+@app.get("/cars")
+def get_cars():
+    return tmpdict
+
+# get car
+@app.get("/car/{car_id}")
+def get_car(car_id: int):
+    return tmpdict[car_id]
+
+# set car of the month
