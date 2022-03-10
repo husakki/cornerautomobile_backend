0fdf684d4f379d1b3860b015c9bae50c73517c0e
husakki <hueschromo@gmail.com>
2022-03-10 09:16:00 +0100

    added inserts and comments

--- mysql_database/corneram_db.sql
+++ mysql_database/corneram_db.sql
@@ -1,3 +1,4 @@
+# The Table
 CREATE TABLE CARS (
 	carid integer,
     brand VARCHAR(30),
@@ -9,4 +10,10 @@ CREATE TABLE CARS (
     constraint pk_car primary key(carid)
 );
 
+# Sample inserts
+INSERT INTO CARS(carid,brand, model, kilometer,consumption,cofm) values(1,"mercedes","a-klasse",10000,10,false);
 INSERT INTO CARS(carid,brand, model, kilometer,consumption,cofm) values(2,"mercedes","a-klasse",10000,10,false);
+INSERT INTO CARS(carid,brand, model, kilometer,consumption,cofm) values(3,"bmw","5er",1000,15,true);
+INSERT INTO CARS(carid,brand, model, kilometer,consumption,cofm) values(4,"audi","a3rs",20000,13,false);
+INSERT INTO CARS(carid,brand, model, kilometer,consumption,cofm) values(5,"ford","kuga",100000,10,true);
+
